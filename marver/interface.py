c97a0b2ada6f4b8980f01072acf80aede536b105
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2016-04-29 17:31:05 +0200

    started implementing marvers main interface, not sure what it will look like

--- marver/interface.py
+++ marver/interface.py
@@ -1,11 +1,15 @@
-#!/usr/bin/env python
+""" Marver's main user interface module
+
+"""
+
+
+def open_file(filename):
+    """just reads the file contents into a string"""
+    with open(filename, 'r') as f:
+        return f.read()
 
 
 
-def 
 
 
 
-
-
-def preprocess(source_code, 
